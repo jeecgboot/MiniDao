2fa50b2cb3ef3e0a42598426ba3fe7e326d992bc
zhangdaiscott <zhangdaiscott@163.com>
2013-11-26 12:50:59 +0800

    1
    1

--- src/examples/sql/user/UserDao_sle.sql
+++
@@ -1,6 +0,0 @@
- select
-        jform_version   
-    from
-        cgform_head    
-    where
-        id = 'jform_order_main'
--- src/org/jeecgframework/minidao/datasource/DataSourceType.java
+++
@@ -1,5 +0,0 @@
-package org.jeecgframework.minidao.datasource;
-
-public enum DataSourceType {
-	dataSource_jeecg,dataSource_enter,dataSource4,mapdataSource
-}
